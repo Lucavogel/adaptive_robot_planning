1fecc10e76dac86bf81bcb0d056101640a24f25d
Lucavogel <lucavogelgesang97@gmail.com>
2025-06-18 14:31:22 +0200

    Created using Colab

---
+++ reasoning_ollama.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMhg9J7Mfs3AiR2vYh64pNW",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Lucavogel/adaptive_robot_planning/blob/main/reasoning_ollama.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "dlzl0d_xBEhM"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
