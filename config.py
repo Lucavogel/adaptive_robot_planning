194b9cca5e954cfa1c9c7f43c8912be213a6b1ed
Lucavogel <lucavogelgesang97@gmail.com>
2025-07-03 12:25:26 +0200

    no

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 BASE_URL="https://openrouter.ai/api/v1"
 API_KEY="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"
+API_KEY="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"
 MODEL="nvidia/llama-3.3-nemotron-super-49b-v1:free"
 #1) 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
 #2) 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
